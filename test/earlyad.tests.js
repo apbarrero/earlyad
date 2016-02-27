878143987f48831453a4f46df28727215f0cb8b3
Antonio Pérez Barrero <apbarrero@gmail.com>
2016-02-27 22:36:22 +0100

    Export constructor function EarlyAd

--- test/earlyad.tests.js
+++ test/earlyad.tests.js
@@ -1,49 +1,45 @@
 #!/usr/bin/env node
 
 var EarlyAd = require('../lib/earlyad');
-var isNewerVersion = EarlyAd.isNewerVersion;
-var checkDepVersion = EarlyAd.checkDepVersion;
-var checkDepRepo = EarlyAd.checkDepRepo;
-var checkDepRepoList = EarlyAd.checkDepRepoList;
-var extractUserRepo = EarlyAd.extractUserRepo;
-var fetchPackageJson = EarlyAd.fetchPackageJson;
 var assert = require('chai').assert;
 
 describe('Early adopter', function() {
 
+   var ea = new EarlyAd({ token: process.env.GITHUB_TOKEN });
+
    describe('isNewerVersion', function() {
 
       it('should return true if new version is greater than current', function() {
-         assert.ok(isNewerVersion('1.2.3', '1.2.2'));
-         assert.ok(isNewerVersion('1.4.1', '1.3.1'));
-         assert.ok(isNewerVersion('2.3.1', '1.3.1'));
-         assert.ok(isNewerVersion('1.4.1', '1.3.12'));
-         assert.ok(isNewerVersion('1.15.0', '1.8.12'));
-         assert.ok(isNewerVersion('2.0.0', '1.8.10'));
-         assert.ok(isNewerVersion('10.0.0', '9.2.18'));
+         assert.ok(ea.isNewerVersion('1.2.3', '1.2.2'));
+         assert.ok(ea.isNewerVersion('1.4.1', '1.3.1'));
+         assert.ok(ea.isNewerVersion('2.3.1', '1.3.1'));
+         assert.ok(ea.isNewerVersion('1.4.1', '1.3.12'));
+         assert.ok(ea.isNewerVersion('1.15.0', '1.8.12'));
+         assert.ok(ea.isNewerVersion('2.0.0', '1.8.10'));
+         assert.ok(ea.isNewerVersion('10.0.0', '9.2.18'));
       });
       it('should return false if new version is lesser than current', function() {
-         assert.notOk(isNewerVersion('1.2.1', '1.2.2'));
-         assert.notOk(isNewerVersion('1.4.1', '1.5.1'));
-         assert.notOk(isNewerVersion('2.3.1', '3.3.1'));
-         assert.notOk(isNewerVersion('1.4.12', '1.5.1'));
-         assert.notOk(isNewerVersion('1.0.15', '1.2.1'));
-         assert.notOk(isNewerVersion('2.9.0', '3.0.10'));
-         assert.notOk(isNewerVersion('9.1.12', '10.0.1'));
+         assert.notOk(ea.isNewerVersion('1.2.1', '1.2.2'));
+         assert.notOk(ea.isNewerVersion('1.4.1', '1.5.1'));
+         assert.notOk(ea.isNewerVersion('2.3.1', '3.3.1'));
+         assert.notOk(ea.isNewerVersion('1.4.12', '1.5.1'));
+         assert.notOk(ea.isNewerVersion('1.0.15', '1.2.1'));
+         assert.notOk(ea.isNewerVersion('2.9.0', '3.0.10'));
+         assert.notOk(ea.isNewerVersion('9.1.12', '10.0.1'));
       });
       it('should return false if any parameter is non-compliant with semantic version', function() {
-         assert.notOk(isNewerVersion('1.2.3', 'foo'));
-         assert.notOk(isNewerVersion('foo', '1.2.3'));
-         assert.notOk(isNewerVersion('foo', 'bar'));
-         assert.notOk(isNewerVersion('1.2.3', '1.0'));
-         assert.notOk(isNewerVersion('1.0', '1.2.3'));
-         assert.notOk(isNewerVersion('1.0', '1.0'));
-         assert.notOk(isNewerVersion('1.2.3', '42'));
-         assert.notOk(isNewerVersion('42', '1.2.3'));
-         assert.notOk(isNewerVersion('42', '42'));
-         assert.notOk(isNewerVersion('1.2.3', '1.2.3foo'));
-         assert.notOk(isNewerVersion('1.2.3foo', '1.2.3'));
-         assert.notOk(isNewerVersion('1.2.3foo', '1.2.3foo'));
+         assert.notOk(ea.isNewerVersion('1.2.3', 'foo'));
+         assert.notOk(ea.isNewerVersion('foo', '1.2.3'));
+         assert.notOk(ea.isNewerVersion('foo', 'bar'));
+         assert.notOk(ea.isNewerVersion('1.2.3', '1.0'));
+         assert.notOk(ea.isNewerVersion('1.0', '1.2.3'));
+         assert.notOk(ea.isNewerVersion('1.0', '1.0'));
+         assert.notOk(ea.isNewerVersion('1.2.3', '42'));
+         assert.notOk(ea.isNewerVersion('42', '1.2.3'));
+         assert.notOk(ea.isNewerVersion('42', '42'));
+         assert.notOk(ea.isNewerVersion('1.2.3', '1.2.3foo'));
+         assert.notOk(ea.isNewerVersion('1.2.3foo', '1.2.3'));
+         assert.notOk(ea.isNewerVersion('1.2.3foo', '1.2.3foo'));
       });
    });
 
@@ -62,7 +58,7 @@ describe('Early adopter', function() {
 
          it('should return updated list of dependencies if dependency is listed within package with a lesser version', function() {
             assert.deepEqual(
-               checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "1.0.1" }),
+               ea.checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "1.0.1" }),
                {
                   "name": "foo",
                   "version": "1.0.0",
@@ -73,7 +69,7 @@ describe('Early adopter', function() {
                }
             );
             assert.deepEqual(
-               checkDepVersion(pack, { url: "git://github.com/baz/bar2.git", version: "1.3.0" }),
+               ea.checkDepVersion(pack, { url: "git://github.com/baz/bar2.git", version: "1.3.0" }),
                {
                   "name": "foo",
                   "version": "1.0.0",
@@ -85,18 +81,18 @@ describe('Early adopter', function() {
             );
          });
          it('should return null if dependency is listed within package with a greater version', function() {
-            assert.isNull(checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "0.9.0" }));
-            assert.isNull(checkDepVersion(pack, { url: "git://github.com/baz/bar2.git", version: "1.1.10" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "0.9.0" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "git://github.com/baz/bar2.git", version: "1.1.10" }));
          });
          it('should return null if dependency is listed within package with the same version', function() {
-            assert.isNull(checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "1.0.0" }));
-            assert.isNull(checkDepVersion(pack, { url: "git://github.com/baz/bar2.git", version: "1.2.3" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "1.0.0" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "git://github.com/baz/bar2.git", version: "1.2.3" }));
          });
          it('should return null if dependency is not included in package dependencies', function() {
-            assert.isNull(checkDepVersion(pack, { url: "git://github.com/baz/notfound.git", version: "1.0.0" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "git://github.com/baz/notfound.git", version: "1.0.0" }));
          });
          it('should return null if dependency version is not valid semantic version', function() {
-            assert.isNull(checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "invalid version" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "invalid version" }));
          });
       });
 
@@ -112,7 +108,7 @@ describe('Early adopter', function() {
 
          it('should return updated list of dependencies if dependency is listed within package with a lesser version', function() {
             assert.deepEqual(
-               checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "1.0.1" }),
+               ea.checkDepVersion(pack, { url: "git://github.com/baz/bar.git", version: "1.0.1" }),
                {
                   "name": "foo",
                   "version": "1.0.0",
@@ -123,7 +119,7 @@ describe('Early adopter', function() {
                }
             );
             assert.deepEqual(
-               checkDepVersion(pack, { url: "git://github.com/baz/bar2.git", version: "1.3.0" }),
+               ea.checkDepVersion(pack, { url: "git://github.com/baz/bar2.git", version: "1.3.0" }),
                {
                   "name": "foo",
                   "version": "1.0.0",
@@ -135,18 +131,18 @@ describe('Early adopter', function() {
             );
          });
          it('should return null if dependency is listed within package with a greater version', function() {
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar", version: "0.9.0" }));
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar2", version: "1.1.10" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar", version: "0.9.0" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar2", version: "1.1.10" }));
          });
          it('should return null if dependency is listed within package with the same version', function() {
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar", version: "1.0.0" }));
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar2", version: "1.2.3" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar", version: "1.0.0" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar2", version: "1.2.3" }));
          });
          it('should return null if dependency is not included in package dependencies', function() {
-            assert.isNull(checkDepVersion(pack, { url: "baz/notfound", version: "1.0.0" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/notfound", version: "1.0.0" }));
          });
          it('should return null if dependency version is not valid semantic version', function() {
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar", version: "invalid version" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar", version: "invalid version" }));
          });
       });
 
@@ -163,12 +159,12 @@ describe('Early adopter', function() {
          };
 
          it('should return null if dependency in package dependencies has no version', function() {
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar", version: "1.2.3" }));
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar3", version: "1.2.3" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar", version: "1.2.3" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar3", version: "1.2.3" }));
          });
          it('should return null if dependency in package dependencies has an invalid semantic version', function() {
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar2", version: "1.2.3" }));
-            assert.isNull(checkDepVersion(pack, { url: "baz/bar4", version: "1.2.3" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar2", version: "1.2.3" }));
+            assert.isNull(ea.checkDepVersion(pack, { url: "baz/bar4", version: "1.2.3" }));
          });
       });
    });
@@ -176,24 +172,24 @@ describe('Early adopter', function() {
    describe('extractUserRepo', function() {
       it('should extract user and repo name from full github URL', function() {
          assert.deepEqual(
-            extractUserRepo('git://github.com/apbarrero/earlyad.git'),
+            ea.extractUserRepo('git://github.com/apbarrero/earlyad.git'),
             { user: 'apbarrero', repo: 'earlyad' }
          );
       });
       it('should extract user and repo name from abbreviated github URL', function() {
          assert.deepEqual(
-            extractUserRepo('apbarrero/earlyad'),
+            ea.extractUserRepo('apbarrero/earlyad'),
             { user: 'apbarrero', repo: 'earlyad' }
          );
       });
       it('should return null if an invalid url is passed', function() {
-         assert.isNull(extractUserRepo('this is not a valid URL'));
+         assert.isNull(ea.extractUserRepo('this is not a valid URL'));
       });
    });
 
    describe('fetchPackageJson', function() {
       it('should return package.json contents for a github repository', function(done) {
-         fetchPackageJson('apbarrero/earlyad', function(err, res) {
+         ea.fetchPackageJson('apbarrero/earlyad', function(err, res) {
             if (err) {
                throw err;
                done();
@@ -212,7 +208,7 @@ describe('Early adopter', function() {
 
       it('should return the repository package object with dependency list properly updated when one dependency needs to be updated', function(done) {
          var newSemver = { url: 'git://github.com/npm/node-semver.git', version: '99.99.99' };
-         checkDepRepo(repo, newSemver, function(err, res) {
+         ea.checkDepRepo(repo, newSemver, function(err, res) {
             if (err) {
                throw err;
                done();
@@ -227,7 +223,7 @@ describe('Early adopter', function() {
       });
       it('should return null if repo doesn\'t include the given dependency', function(done) {
          var newSemver = { url: 'git://github.com/dontdepend/onthis.git', version: '99.99.99' };
-         checkDepRepo(repo, newSemver, function(err, res) {
+         ea.checkDepRepo(repo, newSemver, function(err, res) {
             if (err) {
                throw err;
                done();
@@ -249,7 +245,7 @@ describe('Early adopter', function() {
       describe('when one repo in the list needs to update the dependency', function() {
          it('should return one package object with the updated dependency list', function(done) {
             var newSemver = { url: 'git://github.com/npm/node-semver.git', version: '99.99.99' };
-            checkDepRepoList(repolist, newSemver, function(err, res) {
+            ea.checkDepRepoList(repolist, newSemver, function(err, res) {
                if (err) {
                   throw err;
                   done();
